b3039a817cca2ff3bd9df9d0291701a3778188b5
prhasn <hi@prhasn.com>
2025-04-28 10:35:31 +0700

    TS upgrade

---
+++ dist/index.d.ts
@@ -0,0 +1,67 @@
+import webpack from "webpack";
+import HtmlWebpackPlugin from "html-webpack-plugin";
+import { CleanWebpackPlugin } from "clean-webpack-plugin";
+import WebpackObfuscator from "webpack-obfuscator";
+import CopyWebpackPlugin from "copy-webpack-plugin";
+import * as SitemapPlugin from "sitemap-webpack-plugin";
+import CssMinimizerPlugin from "css-minimizer-webpack-plugin";
+import MiniCssExtractPlugin from "mini-css-extract-plugin";
+import TerserPlugin from "terser-webpack-plugin";
+import { Config } from "./types";
+declare const build: ({ mode, appShortName, websiteName, websiteDomain, publishedTime, author, websiteTitle, websiteDescription, coverImage, coverImageDescription, notificationTitle, notificationText, background_color, theme_color, app_icon, orientation, pagesList, htmlCommonElements, obfuscateON, srcDir, assetsDir, developDir, commonDir, imagesDir, pagesDir, pageHome, productionDir, }: Config) => {
+    entry: {
+        [x: string]: string;
+    };
+    performance: {
+        maxAssetSize: number;
+        maxEntrypointSize: number;
+    };
+    output: {
+        path: string;
+        filename: string;
+    };
+    resolve: {
+        extensions: string[];
+    };
+    module: {
+        rules: ({
+            test: RegExp;
+            exclude: RegExp;
+            use: string;
+            type?: undefined;
+            include?: undefined;
+            generator?: undefined;
+        } | {
+            test: RegExp;
+            exclude: RegExp;
+            use: string[];
+            type?: undefined;
+            include?: undefined;
+            generator?: undefined;
+        } | {
+            test: RegExp;
+            exclude: RegExp;
+            type: string;
+            include: string;
+            generator: {
+                filename: string;
+            };
+            use?: undefined;
+        })[];
+    };
+    plugins: (HtmlWebpackPlugin | WebpackObfuscator | CopyWebpackPlugin | MiniCssExtractPlugin | webpack.DefinePlugin | CleanWebpackPlugin | SitemapPlugin.default | import("html-inline-css-webpack-plugin/build/core").PluginForHtmlWebpackPluginV3 | import("html-inline-css-webpack-plugin/build/core").PluginForHtmlWebpackPluginV4)[];
+    optimization: {
+        minimize: boolean;
+        minimizer: (TerserPlugin<import("terser").MinifyOptions> | CssMinimizerPlugin<CssMinimizerPlugin.CssNanoOptionsExtended>)[];
+    };
+    devServer: {
+        static: {
+            directory: string;
+        };
+        port: number;
+        open: boolean;
+        compress: boolean;
+    };
+    mode: "development" | "production";
+};
+export { build };
---
+++ types.ts
@@ -0,0 +1,70 @@
+export interface StringObj {
+    [key: string]: string;
+}
+
+interface Page {
+    uri: string;
+    name: string;
+    short_name: string;
+    description: string;
+    icon: string;
+    shortcut: boolean;
+}
+
+export interface Config {
+    mode: 'development' | 'production';
+    appShortName: string;
+    websiteName: string;
+    websiteDomain: string;
+    publishedTime: string;
+    author: string;
+    websiteTitle: string;
+    websiteDescription: string;
+    coverImage: string;
+    coverImageDescription: string;
+    notificationTitle: string;
+    notificationText: string;
+    background_color: string;
+    theme_color: string;
+    app_icon: string;
+    orientation: 'portrait' | 'landscape';
+    pagesList: Page[];
+    htmlCommonElements: string[];
+    obfuscateON: boolean;
+    srcDir: string;
+    assetsDir: string;
+    developDir: string;
+    commonDir: string;
+    imagesDir: string;
+    pagesDir: string;
+    pageHome: string;
+    productionDir: string;
+}
+
+interface Icon {
+    src: string;
+    type: string;
+    sizes: string;
+    purpose: 'maskable' | 'any';
+}
+
+interface Shortcut {
+    name: string;
+    short_name: string;
+    description: string;
+    url: string;
+    icons: Icon[];
+}
+
+export interface WebManifest {
+    background_color: string;
+    theme_color: string;
+    icons: Icon[];
+    shortcuts: Shortcut[];
+    display: string;
+    orientation: 'portrait' | 'landscape';
+    short_name: string;
+    start_url: string;
+    description: string;
+    name: string;
+}
